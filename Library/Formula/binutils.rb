b7b045721b55f5b84ce3689888e6558b0d23e622
Dmitry Nedospasov <dmitry@nedos.net>
2011-12-21 14:52:02 +0100

    binutils: add options for libbfd and libiberty

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -10,6 +10,8 @@ class Binutils < Formula
     [['--default-names', "Do NOT prepend 'g' to the binary; will override system utils."]]
   end
 
+  def patches; DATA; end
+
   def install
     args = ["--disable-debug", "--disable-dependency-tracking",
             "--prefix=#{prefix}",
@@ -23,7 +25,43 @@ class Binutils < Formula
     args << "--program-prefix=g" unless ARGV.include? '--default-names'
 
     system "./configure", *args
+
+    if ARGV.include? '--with-libiberty'
+      Dir.chdir "libiberty" do
+        system "./configure", "--enable-install-libiberty", *args
+        system "make"
+        system "make install"
+      end
+    end
+
+    if ARGV.include? '--with-bfd'
+      Dir.chdir "bfd" do
+        system "./configure", "--enable-install-libbfd", *args
+        system "make"
+        system "make install"
+      end
+    end
+
+
     system "make"
     system "make install"
   end
 end
+
+__END__
+diff --git a/libiberty/Makefile.in b/libiberty/Makefile.in
+index 7a8bb02..640dc7a 100644
+--- a/libiberty/Makefile.in
++++ b/libiberty/Makefile.in
+@@ -346,7 +346,10 @@ install: install_to_$(INSTALL_DEST) install-subdir
+ # multilib-specific flags, it's overridden by FLAGS_TO_PASS from the
+ # default multilib, so we have to take CFLAGS into account as well,
+ # since it will be passed the multilib flags.
+-MULTIOSDIR = `$(CC) $(CFLAGS) -print-multi-os-directory`
++
++# Comment out MULTIOSDIR - this breaks $(CC) -libiberty
++#MULTIOSDIR = `$(CC) $(CFLAGS) -print-multi-os-directory`
++MULTIOSDIR =
+ install_to_libdir: all
+        ${mkinstalldirs} $(DESTDIR)$(libdir)/$(MULTIOSDIR)
+        $(INSTALL_DATA) $(TARGETLIB) $(DESTDIR)$(libdir)/$(MULTIOSDIR)/$(TARGETLIB)n
